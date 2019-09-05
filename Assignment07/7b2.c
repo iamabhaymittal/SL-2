35214e66fba11539d33bf48a48abf1642dc85f02
nllab20 <iamabhaymittal@gmail.com>
2019-09-05 10:59:58 +0530

    added as8

--- Assignment07/7b2.c
+++ Assignment07/7b2.c
@@ -1,3 +1,6 @@
+// \n is extremely important.
+
+
 
 #include <stdio.h> 
 #include <string.h> 
@@ -10,18 +13,33 @@ int main()
 { 
 	int fd1; 
 
+	int c_w=0,c_s=0;
 	char * myfifo = "/tmp/myfifo"; 
 
 	mkfifo(myfifo, 0666); 
 
-	char str1[80], str2[80]; 
+	char str1[80]; 
 	while (1) 
 	{ 
+		c_w=0;
+		c_s=0;
 		fd1 = open(myfifo,O_RDONLY); 
 		read(fd1, str1, 80); 
 		
+		for(int i=0;i<(strlen(str1)-1);i++)
+		{
+			if(str1[i] == ' ')
+				c_w++;
+		}
+		for(int i=0;i<(strlen(str1)-1);i++)
+		{
+			if(str1[i] == '.')
+				c_s++;
+		}
+		printf("Number of characters: %d  ",(strlen(str1)-1));
+		printf("Number of words: %d  ",c_w+1);
+		printf("Number of sentences: %d\n\n",c_s);
 		
-		printf("\nLength of Sentence: %d",(strlen(str1)-1));
 		
 		
 	} 
